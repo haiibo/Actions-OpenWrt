c3944a05c2b395448a231c95e3fe16a11835cbb4
buy404 <99722055+buy404@users.noreply.github.com>
2024-11-30 13:54:09 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -268,7 +268,6 @@ status
 
 #CACHE_URL=$(curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
 curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | grep -oP 'download_url": "\K[^"]*cache[^"]*' >cache_url
-
 if (grep -q "$CACHE_NAME" cache_url); then
     STEP_NAME='下载toolchain编译工具'; BEGIN_TIME=$(date '+%H:%M:%S')
     wget -qc -t=3 $(grep "$CACHE_NAME" cache_url)
