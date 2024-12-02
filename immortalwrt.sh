0646609b2b8e3dbbe65816d16c1682559f853af7
buy404 <99722055+buy404@users.noreply.github.com>
2024-12-02 14:03:33 +0800

    Update immortalwrt.sh

--- immortalwrt.sh
+++ immortalwrt.sh
@@ -178,6 +178,7 @@ config () {
 			# CONFIG_GRUB_EFI_IMAGES is not set
 			# CONFIG_VMDK_IMAGES is not set
 			EOF
+   			KERNEL_TARGET=amd64
 			;;
 		"r1-plus-lts"|"r1-plus"|"r4s"|"r2c"|"r2s")
 			cat >.config<<-EOF
@@ -196,6 +197,7 @@ config () {
 			"r4s"|"r2c"|"r2s")
 			echo "CONFIG_TARGET_rockchip_armv8_DEVICE_friendlyarm_nanopi-$TARGET_DEVICE=y" >>.config ;;
 			esac
+   			KERNEL_TARGET=arm64
 			;;
 		"newifi-d2")
 			cat >.config<<-EOF
@@ -240,6 +242,7 @@ config () {
 				CONFIG_TARGET_armsr_armv8_DEVICE_generic=y
 				EOF
 			fi
+      			KERNEL_TARGET=arm64
 			;;
 	esac
 }
